d0c63e8ec48d5868c24df3c794b6a30c4d005dd6
naitiaz-aya <bntsaidaya@gmail.com>
2021-12-01 12:12:51 +0100

    aya update

--- JavaScript/questions.js
+++ JavaScript/questions.js
@@ -1,31 +1,52 @@
 class Question {
   constructor(
-    id,
-    question, 
-    subjectid, 
+    question,
+    subjectid,
     probableanswerid,
     correctid,
     points,
     levelid,
-  ){
-    this.id = id
-    this.question = question
-    this.subjectid = subjectid
-    this.probableanswerid = probableanswerid
-    this.correctid = correctid
-    this.points = points 
-    this.levelid = levelid
-  }
-  
-  addQuestion(){
-    console.log('Question added!')
+    id
+  ) {
+    this.question = question;
+    this.subjectid = subjectid;
+    this.probableanswerid = probableanswerid;
+    this.correctid = correctid;
+    this.points = points;
+    this.levelid = levelid;
+    this.id = id;
   }
 
-  correctAnswer(){
-    console.log('The correct answer')
+  addQuestion() {
+        var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    var raw = JSON.stringify({
+      "id": this.id,
+      "question": this.question,
+      "subjectid": this.subjectid,
+      "probableanswersid": this.probableanswerid,
+      "correctid": this.correctid,
+      "points": this.points,
+      "level": this.levelid
+    });
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    fetch("http://localhost:3000/question", requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+    console.log("question added")
   }
 
-  answerByQuestion(){
-    console.log('All answers probable!')
+  correctAnswer() {
+    console.log("The correct answer");
+  }
+
+  answerByQuestion() {
+    console.log("All answers probable!");
   }
 }
